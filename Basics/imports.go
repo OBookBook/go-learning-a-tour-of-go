5c680abca9de2cfd1214d2431668bc91af06c9c3
N.I <iworksWeb@gmail.com>
2023-05-09 23:29:06 +0900

    Basics 2/17 finish

---
+++ Basics/imports.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	// RUN Now you have 2.6457513110645907 problems.
+}
--- Basics/packages.go
+++ Basics/packages.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	// pseudo-random number
 	fmt.Println("My favorite number is", rand.Intn(10))
+	//  出力結果 My favorite number is 8
 }
