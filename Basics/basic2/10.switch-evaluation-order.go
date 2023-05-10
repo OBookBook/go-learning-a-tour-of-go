e42bdc737e89feb0a1d29481d87fb4777fb185ea
N.I <iworksWeb@gmail.com>
2023-05-11 02:06:55 +0900

    Basics2 Switch evaluation order 10/14 finish

---
+++ Basics/basic2/10.switch-evaluation-order.go
@@ -0,0 +1,27 @@
+/*
+Switch evaluation order
+https://go-tour-jp.appspot.com/flowcontrol/10
+
+switch caseは、上から下へcaseを評価します。 caseの条件が一致すれば、そこで停止(自動的にbreak)します。
+*/
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
