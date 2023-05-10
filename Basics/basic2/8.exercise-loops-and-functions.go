1128da30000153219d219605fa05e2f3a22cbb91
N.I <iworksWeb@gmail.com>
2023-05-11 01:58:06 +0900

    Basics2 Exercise: Loops and Functions 8/14 finish

---
+++ Basics/basic2/8.exercise-loops-and-functions.go
@@ -0,0 +1,24 @@
+/*
+Exercise: Loops and Functions
+https://go-tour-jp.appspot.com/flowcontrol/8
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(math.Sqrt(2))
+}
