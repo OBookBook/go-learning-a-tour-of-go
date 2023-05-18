d7d35e92b1cdd5e04b76dca088e6a63ba470ac08
N.I <iworksWeb@gmail.com>
2023-05-18 14:04:22 +0900

    methods Pointers and functions 5/26 finish

---
+++ methods/5.methods-pointers-explained.go
@@ -0,0 +1,33 @@
+/*
+Pointers and functions
+https://go-tour-jp.appspot.com/methods/5
+
+実行コマンド
+go run methods/5.methods-pointers-explained.go
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Scale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	Scale(&v, 10)
+	fmt.Println(Abs(v)) // 50
+}
