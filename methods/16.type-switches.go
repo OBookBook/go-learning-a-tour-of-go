77ea9ea3a994fbcddef85931ab08951a4390868b
N.I <iworksWeb@gmail.com>
2023-05-21 02:25:19 +0900

    methods Type switches 16/26 finish

---
+++ methods/16.type-switches.go
@@ -0,0 +1,30 @@
+/*
+Type switches
+https://go-tour-jp.appspot.com/methods/16
+
+型switch はいくつかの型アサーションを直列に使用できる構造です。
+
+実行コマンド
+go run methods/16.type-switches.go
+*/
+
+package main
+
+import "fmt"
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
+
+func main() {
+	do(21)      // Twice 21 is 42
+	do("hello") // "hello" is 5 bytes long
+	do(true)    // I don't know about type bool!
+}
